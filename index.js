3300c3f3d90a853335c97157c2c127f74ca135be
“[Montel <[mhsports345@gmail.com]>
2023-11-06 10:38:35 -0500

    built css page, started building grids

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-index.js
+document.addEventListener('DOMContentLoaded', function() {
+    const gridContainer = document.querySelector('.grid-container');
+
+    for (let i = 0; i < 16 * 16; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        gridContainer.appendChild(div);
+    }
+});
