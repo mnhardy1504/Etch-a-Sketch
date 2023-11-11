ec1673a7716de810178d0bb97d7b1ebf01a7b5c6
“[Montel <[mhsports345@gmail.com]>
2023-11-11 11:17:24 -0500

    Hover functionality is working.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gridContainer = document.querySelector('.grid-container');
+document.addEventListener('DOMContentLoaded', drawGrid);
 
+function drawGrid(){
+    const gridContainer = document.querySelector('.grid-container'); 
+    console.log('hey');
     for (let i = 0; i < 16 * 16; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
+        div.addEventListener('mouseenter', mouseHoverStart);
+        div.addEventListener('mouseleave', mouseHoverEnd);
         gridContainer.appendChild(div);
+        console.log('hey whats up', div);
     }
-});
-
-const gridDivs = document.querySelectorAll('.grid-item');  
+}
 
 function mouseHoverStart(event) {
     event.target.style.backgroundColor = 'red';
+    console.log('this works');
 }
 
 function mouseHoverEnd(event) {
     event.target.style.backgroundColor ='lightgray';
+    console.log('new statement');
 }
-
-gridDivs.forEach(function(div) {
-    div.addEventListener('mouseenter', mouseHoverStart);
-    div.addEventListener('mouseleave', mouseHoverEnd);
-});
