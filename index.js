49f6d2c0a8063768772fdc0dfef5f783c74e0594
“[Montel <[mhsports345@gmail.com]>
2023-11-13 17:33:24 -0500

    added working pop up window for input

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-document.addEventListener('DOMContentLoaded', drawGrid);
 
-function drawGrid(){
-    const gridContainer = document.querySelector('.grid-container'); 
-    console.log('hey');
-    for (let i = 0; i < 16 * 16; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid-item');
-        div.addEventListener('mouseenter', mouseHoverStart);
-        div.addEventListener('mouseleave', mouseHoverEnd);
-        gridContainer.appendChild(div);
-        console.log('hey whats up', div);
-    }
+function enterGrid() {
+    let size = prompt("What number of squares per side do you want for each side of the grid?", 7);
+    document.documentElement.style.setProperty('--num-columns', size); 
+    const gridContainer = document.getElementById('grid-container'); 
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild); 
+    };
+
+    for (let i = 0; i < size * size; i++ ) {
+       const div = document.createElement('div');
+       div.classList.add('grid-item');
+       div.addEventListener('mouseenter', mouseHoverStart);
+       div.addEventListener('mouseleave', mouseHoverEnd);
+       gridContainer.appendChild(div);
+     }
 }
+
+
+//document.addEventListener('DOMContentLoaded', enterGrid);
 
 function mouseHoverStart(event) {
     event.target.style.backgroundColor = 'red';
-    console.log('this works');
 }
 
 function mouseHoverEnd(event) {
     event.target.style.backgroundColor ='lightgray';
-    console.log('new statement');
 }
